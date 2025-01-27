e601de9d17bbfb893e2fc1993ddfa4426b3f055d
Ziyan Cheng <ziyan.cheng@outlook.it>
2025-01-27 23:25:13 +0100

    1

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -187,8 +187,8 @@ docker run -d -it \
     -v ${USERS_JSON_PATH}:/etc/v2ray/users.json \
     -v ${CONFIG_JSON_PATH}:/etc/v2ray/config.json \
     -v ${LOG_FILE_PATH}:/var/log/v2ray \
-    -p ${VMESS_PORT}:${VMESS_PORT} \  
+    -p ${VMESS_PORT}:${VMESS_PORT} \
     --restart always \
     --ip 172.17.0.101 \
     --name v2ray \
-    ziyan1c/v2ray 
+    ziyan1c/v2ray
